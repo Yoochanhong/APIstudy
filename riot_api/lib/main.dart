b35a519ff3c7f53fa68a6fbc1d408f264c4ba558
Yoochanhong <chanhongy6@naver.com>
2022-09-25 23:03:16 +0900

    FutureBuilder 구현

--- riot_api/lib/main.dart
+++ riot_api/lib/main.dart
@@ -9,6 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: MyHomePage(),
     );
   }
@@ -20,6 +21,7 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  Future<Info>? info;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -27,18 +29,25 @@ class _MyHomePageState extends State<MyHomePage> {
         title: Text('라이엇 일병 구하기'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () {
+              fetchInfo();
+            },
             icon: Icon(Icons.refresh),
           ),
         ],
       ),
       body: Container(
         child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(''),
-            ],
+          child: FutureBuilder<Info>(
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                return Text(snapshot.data!.name.toString());
+              } else if (snapshot.hasError){
+                return Text("에러일때");
+              } else {
+                return Text("로딩 화면");
+              }
+            },
           ),
         ),
       ),
@@ -49,7 +58,8 @@ class _MyHomePageState extends State<MyHomePage> {
 Future<Info> fetchInfo() async {
   final response = await http.get(Uri.parse(
       'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%EC%9C%A0%EC%B0%AC%ED%99%8D?api_key=RGAPI-39d9ecc4-47cc-4c46-aba1-5ba3fcf7214d'));
-  if (response.statusCode == 200){
+  if (response.statusCode == 200) {
+    print(json.decode(response.body));
     return Info.fromjson(json.decode(response.body));
   } else {
     throw Exception('실패');
@@ -57,15 +67,15 @@ Future<Info> fetchInfo() async {
 }
 
 class Info {
-  String? name;
-  int? level;
+  String name;
+  int summonerLevel;
 
-  Info({this.name, this.level});
+  Info({required this.name, required this.summonerLevel});
 
   factory Info.fromjson(Map<dynamic, dynamic> json) {
     return Info(
       name: json['name'],
-      level: json['level'],
+      summonerLevel: json['summonerLevel'],
     );
   }
 }
