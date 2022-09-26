5ddca05c2d83343da4062eb323750c64344463c8
Yoochanhong <chanhongy6@naver.com>
2022-09-26 23:04:38 +0900

    라이엇 api 따오기

--- riot_api/lib/main.dart
+++ riot_api/lib/main.dart
@@ -1,7 +1,50 @@
 import 'dart:convert';
-
+import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+
+Future<Info> fetchInfo() async {
+  final response = await http.get(Uri.parse(
+      'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%EC%9C%A0%EC%B0%AC%ED%99%8D?api_key=RGAPI-c1cb31ba-db37-47aa-a42c-d796c1351b69'));
+  if (response.statusCode == 200) {
+    print(json.decode(response.body));
+    return Info.fromjson(json.decode(response.body));
+  } else {
+    throw Exception('실패');
+  }
+}
+
+class Info {
+  String? id;
+  String? accountId;
+  String? puuid;
+  String? name;
+  int? profileIconId;
+  int? revisionDate;
+  int? summonerLevel;
+
+  Info(
+      {this.id,
+        this.accountId,
+        this.puuid,
+        this.name,
+        this.profileIconId,
+        this.revisionDate,
+        this.summonerLevel});
+
+  factory Info.fromjson(Map<dynamic, dynamic> json) {
+    return Info(
+      id: json['id'],
+      accountId: json['accountId'],
+      puuid: json['puuid'],
+      name: json['name'],
+      profileIconId: json['profileIconId'],
+      revisionDate: json['revisionDate'],
+      summonerLevel: json['summonerLevel'],
+    );
+  }
+}
+
 
 void main() => runApp(MyApp());
 
@@ -22,6 +65,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   Future<Info>? info;
+
+  @override
+  void initState() {
+    super.initState();
+    info = fetchInfo();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -40,42 +90,19 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Center(
           child: FutureBuilder<Info>(
             builder: (context, snapshot) {
+              print("snapshot");
+              print(snapshot);
               if (snapshot.hasData) {
                 return Text(snapshot.data!.name.toString());
-              } else if (snapshot.hasError){
-                return Text("에러일때");
+              } else if (snapshot.hasError) {
+                return Text('${snapshot.error}');
               } else {
-                return Text("로딩 화면");
+                return CircularProgressIndicator();
               }
             },
           ),
         ),
       ),
-    );
-  }
-}
-
-Future<Info> fetchInfo() async {
-  final response = await http.get(Uri.parse(
-      'https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/%EC%9C%A0%EC%B0%AC%ED%99%8D?api_key=RGAPI-39d9ecc4-47cc-4c46-aba1-5ba3fcf7214d'));
-  if (response.statusCode == 200) {
-    print(json.decode(response.body));
-    return Info.fromjson(json.decode(response.body));
-  } else {
-    throw Exception('실패');
-  }
-}
-
-class Info {
-  String name;
-  int summonerLevel;
-
-  Info({required this.name, required this.summonerLevel});
-
-  factory Info.fromjson(Map<dynamic, dynamic> json) {
-    return Info(
-      name: json['name'],
-      summonerLevel: json['summonerLevel'],
     );
   }
 }
